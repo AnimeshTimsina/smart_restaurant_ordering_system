a755b2e37a4015f6584ea987101297e6428dd6b8
Lakshya <lakshyapandit2054@gmail.com>
2019-07-02 10:27:27 +0545

    6

--- resturant/admin.py
+++ resturant/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Table,Type,Food,Customer,Orders
 
 # Register your models here.
+
+admin.site.register(Table)
+admin.site.register(Type)
+admin.site.register(Food)
+admin.site.register(Customer)
+admin.site.register(Orders)
