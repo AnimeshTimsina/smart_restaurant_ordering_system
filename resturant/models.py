a469443de7eb63ac5673e16a2512cef1204fd396
Lakshya <lakshyapandit2054@gmail.com>
2019-07-02 09:46:10 +0545

    first commit

---
+++ resturant/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField,JSONField
+
+
+# Create your models here.
+class Table(models.Model):
+    type_choices = (
+        ('1','booked'),
+        ('2','reserved'),
+        ('3','free'),
+    )
+    name=models.CharField(max_length=50)
+    choice_type = models.CharField(max_length=1,choices = type_choices)
+
+class Type(models.Model):
+    name=models.CharField(max_length=50)
+    choice=models.BooleanField()
+
+class Food(models.Model):
+    name=models.CharField(max_length=50)
+    pricePerQuantity=models.IntegerField()
+    type=models.ForeignKey(Type,on_delete=models.CASCADE)
+    rating=JSONField()
+    orderCount=models.IntegerField()
+    description=models.CharField(max_length=100)
+    imageUrl=models.URLField()
+
+class Customer(models.Model):
+    mac_id=models.CharField(max_length=50)
+
+class Orders(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    table=models.ForeignKey(Table,on_delete=models.CASCADE)
+    food=models.ManyToManyField(Food)
+    quantity=ArrayField(models.IntegerField())
+    dateOfCreation=models.DateTimeField()
+    costOfThisOrder=ArrayField(models.IntegerField())
