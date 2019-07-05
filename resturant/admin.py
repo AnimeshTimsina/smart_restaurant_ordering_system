8ddc98a993813c2bbf31225e8ebc27fa4a363807
Animesh Timsina <antim.timsina7777@gmail.com>
2019-07-05 08:51:54 +0545

    V2.0

--- resturant/admin.py
+++ resturant/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
-from .models import Table,Type,Food,Customer,Orders
+from .models import Table,Type,Food,Customer,Orders,MyUser
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
+from .forms import UserCreationForm
+
+class UserAdmin(BaseUserAdmin):
+	add_form = UserCreationForm
+
+	list_display = ('username','email','is_admin')
+	list_filter = ('is_admin',)
+
+	fieldsets = (
+			(None, {'fields': ('username','email','password')}),
+			('Permissions', {'fields': ('is_admin',)})
+		)
+	search_fields = ('username','email')
+	ordering = ('username','email')
+
+	filter_horizontal = ()
 # Register your models here.
 
+admin.site.register(MyUser, UserAdmin)
 admin.site.register(Table)
 admin.site.register(Type)
 admin.site.register(Food)
 admin.site.register(Customer)
 admin.site.register(Orders)
+admin.site.unregister(Group)
--- smartResturant/urls.py
+++ smartResturant/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path,include
+from resturant.views_authentication import register, login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('management/',include('resturant.urls_management')),
     path('customer/',include('resturant.urls_customer')),
+    url(r'^register/$', register),
+    url(r'^login/$', login_view),
+    url(r'^logout/$', logout_view),
 ]
